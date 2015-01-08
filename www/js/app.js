40ea74276b289c797daaa4d671c70d25e81faa3d
Rony Cohen <cohen.rony.pro@gmail.com>
2015-01-08 16:58:15 +0100

    init2 ca merde

--- www/js/app.js
+++ www/js/app.js
@@ -12,7 +12,7 @@ angular.module('ionicApp', ['ionic'])
             })
 
             .state('slide1', {
-                url: '/',
+                url: '/slide1',
                 views: {
                     'slide1-tab': {
                         templateUrl: "templates/slide1.html",
@@ -22,7 +22,7 @@ angular.module('ionicApp', ['ionic'])
             })
 
             .state('slide2', {
-                url: '/',
+                url: '/slide2',
                 views: {
                     'slide2-tab': {
                         templateUrl: "templates/slide2.html",
@@ -32,7 +32,7 @@ angular.module('ionicApp', ['ionic'])
             })
 
             .state('slide3', {
-                url: '/',
+                url: '/slide3',
                 views: {
                     'slide3-tab': {
                         templateUrl: "templates/slide3.html",
@@ -42,7 +42,7 @@ angular.module('ionicApp', ['ionic'])
             })
 
             .state('slide4', {
-                url: '/',
+                url: '/slide4',
                 views: {
                     'slide4-tab': {
                         templateUrl: "templates/slide4.html",
@@ -55,7 +55,8 @@ angular.module('ionicApp', ['ionic'])
     })
 
     .controller('SlidesCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
-
+        console.info('SlidesCtrl');
+        
         // Called each time the slide changes
         $scope.slideChanged = function(index) {
             $scope.slideIndex = index;
@@ -68,18 +69,25 @@ angular.module('ionicApp', ['ionic'])
     })
 
     .controller('Slide1Ctrl', function($scope) {
+        console.info('Slide1Ctrl');
+
         $scope.value = "Slide1";
     })
 
     .controller('Slide2Ctrl', function($scope) {
+        console.info('Slide2Ctrl');
+
         $scope.value = "Slide2";
     })
 
-
     .controller('Slide3Ctrl', function($scope) {
+        console.info('Slide3Ctrl');
+
         $scope.value = "Slide3";
     })
 
     .controller('Slide4Ctrl', function($scope) {
+        console.info('Slide4Ctrl');
+
         $scope.value = "Slide4";
     });
